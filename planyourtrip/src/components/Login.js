388b0788cbd7322ae429f6005781a31fa0f880cd
YangjinHuang95 <yangjinh1995us@gmail.com>
2018-12-15 19:46:08 -0500

    initial the webpag
    initial the webpage, create the register and login class

---
+++ planyourtrip/src/components/Login.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Form, Icon, Input, Button
+} from 'antd';
+import {API_ROOT} from "../constants"
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login">
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{ required: true, message: 'Please input your username!' }],
+          })(
+            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{ required: true, message: 'Please input your Password!' }],
+          })(
+            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+          )}
+        </FormItem>
+        <FormItem>
+          <Button type="primary" htmlType="submit" className="login-form-button">
+            Log in
+          </Button>
+          Or <a href={API_ROOT+'/signup'}>register now!</a>
+        </FormItem>
+      </Form>
+    );// here the `href` need to modify to our project site, just placeholder now
+  }
+}
+
+export const Login = Form.create()(NormalLoginForm);
