4a7c7121319e17cda4dcfa58b0301baaa3e38a57
YangjinHuang95 <yangjinh1995us@gmail.com>
2018-12-22 16:53:34 -0500

    basicly build search page
    basicly build search page

--- planyourtrip/src/components/City.js
+++ planyourtrip/src/components/City.js
@@ -1,60 +1,38 @@
+import {Button, Input} from 'antd';
 import React from 'react';
-import {
-  Layout, Menu, Breadcrumb, Icon,
-} from 'antd';
-import {Link} from "react-router-dom"
+import PropTypes from "prop-types";
+
+const Search = Input.Search;
 
 
 export class City extends React.Component {
-  render() {
-    const { SubMenu } = Menu;
-    const {
-      Content, Footer, Sider,
-    } = Layout;
 
+  //the method begin from here
+  static contextTypes = {
+    router: PropTypes.object
+  }
+  constructor(props, context) {
+    super(props, context);
+  }
+
+
+  //this method is not safe in the future!!!
+  handleSearch = (value) => {
+    console.log(value)
+    this.context.router.history.push("/plan");
+
+  }
+
+  render() {
     return(
-      <Layout className='City'>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>City Search</Breadcrumb.Item>
-          </Breadcrumb>
-          <Layout className='ShowContent' style={{ padding: '24px 0', background: '#fff'}}>
-            <Sider width={350} style={{ background: '#fff' }}>
-              <Menu
-                mode="inline"
-                defaultSelectedKeys={['1']}
-                defaultOpenKeys={['sub1']}
-                style={{ height: '100%' }}
-              >
-                <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
-                  <Menu.Item key="1">option1</Menu.Item>
-                  <Menu.Item key="2">option2</Menu.Item>
-                  <Menu.Item key="3">option3</Menu.Item>
-                  <Menu.Item key="4">option4</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
-                  <Menu.Item key="5">option5</Menu.Item>
-                  <Menu.Item key="6">option6</Menu.Item>
-                  <Menu.Item key="7">option7</Menu.Item>
-                  <Menu.Item key="8">option8</Menu.Item>
-                </SubMenu>
-                <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
-                  <Menu.Item key="9">option9</Menu.Item>
-                  <Menu.Item key="10">option10</Menu.Item>
-                  <Menu.Item key="11">option11</Menu.Item>
-                  <Menu.Item key="12">option12</Menu.Item>
-                </SubMenu>
-              </Menu>
-            </Sider>
-            <Content style={{ padding: '0 24px', minHeight: 280 }}>
-              The whole main part of this page need to be modified.
-            </Content>
-          </Layout>
-        </Content>
-        <Footer className='ShowFooter' style={{ textAlign: 'center' }}>
-          Plan Your Trip @DaguaishouTeam
-        </Footer>
-      </Layout>
+      <div className='City'>
+      <Search
+        placeholder="Input your dream city!!!"
+        enterButton="Search"
+        size="large"
+        onSearch={this.handleSearch}
+      />
+      </div>
     );
   }
 }
